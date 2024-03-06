fef6d7f3a56c9ae24da7f36459c022dfea4dea44
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2024-03-06 06:31:55 +0100

    upload

---
+++ src/pages/contact.tsx
@@ -0,0 +1,58 @@
+// src/components/ContactForm.tsx
+import React, { useState } from 'react';
+// import styles from '@/styles/ContactForm.module.css';
+import styles from '@/src/styles/ContactForm.module.css';
+
+interface FormData {
+  name: string;
+  email: string;
+  message: string;
+}
+
+const Contact: React.FC = () => {
+  const [formData, setFormData] = useState<FormData>({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log('Form submitted:', formData);
+  };
+
+  return (
+    <div className='main' style={{height:"100vh", display:"flex", alignItems:"center"}}>
+    <div className={styles.card}>
+      <form className={styles.contactForm} onSubmit={handleSubmit}>
+        <label className={styles.formLabel}>
+          Name:
+          <input type="text" name="name" value={formData.name} onChange={handleChange} className={styles.inputField} />
+        </label>
+        <label className={styles.formLabel}>
+          Email:
+          <input type="email" name="email" value={formData.email} onChange={handleChange} className={styles.inputField} />
+        </label>
+        <label className={styles.formLabel}>
+          Message:
+          <textarea name="message" value={formData.message} onChange={handleChange} className={styles.textareaField} />
+        </label>
+        <button type="submit" className={styles.submitButton}>
+          Submit
+        </button>
+      </form>
+    </div>
+    </div>
+  );
+};
+
+export default Contact;
