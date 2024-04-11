8b0f33240393810b87bafd748bbf320f8e2b993c
Sebastian Hans Weissenborn <weissenborn24seb@gmail.com>
2024-04-11 15:03:14 +0200

    next

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,16 +1,16 @@
-// import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-// export default function Document() {
-//   return (
-//     <Html lang="en">
-//       <Head />
-//       <body>
-//         <Main />
-//         <NextScript />
-//       </body>
-//     </Html>
-//   );
-// }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
 
-// // ### n a Next.js application, the _document.js file is used to customize the initial HTML document that is served to the client.
-// // The Document component from next/document allows you to modify the <html>, <head>, and <body> tags of the rendered HTML document.
+// ### n a Next.js application, the _document.js file is used to customize the initial HTML document that is served to the client.
+// The Document component from next/document allows you to modify the <html>, <head>, and <body> tags of the rendered HTML document.
