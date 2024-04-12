739627217797e22d3ed502bb8926783809c9a4d5
Sebastian Hans Weissenborn <weissenborn24seb@gmail.com>
2024-04-12 09:13:13 +0200

    next

--- client/src/utils/authHocs/isAuthPublic.tsx
+++ client/src/utils/authHocs/isAuthPublic.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
-import { useSession } from 'next-auth/react';
 
 const IsAuthPublic = (WrappedComponent) => (props) => {
-  const { data: session } = useSession();
 
   return (
     <div>
