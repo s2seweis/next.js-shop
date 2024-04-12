cb95781de7f3922f6e9f56d208ec64d024e728af
Sebastian Hans Weissenborn <weissenborn24seb@gmail.com>
2024-04-12 08:27:57 +0200

    remove small error

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -10,12 +10,10 @@ const NotFoundPage: NextPage = () => {
       <div style={{ display: 'grid' }}>
         <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
         <p className="text-lg mb-4">
-          Oops The page you are looking for does not exist.
+          Oops! The page you are looking for does not exist.
         </p>
         <Link href="/">
-          <span className="text-blue-600 underline cursor-pointer">
-            Go back to Home Page
-          </span>
+          <a className="text-blue-600 underline cursor-pointer">Go back to Home Page</a>
         </Link>
       </div>
     </div>
