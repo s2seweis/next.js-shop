81eb589a5bacbc9a3e26fda234499997abdb9d2e
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2024-04-17 14:43:17 +0200

    next

--- client/src/utils/authHocs/isAuthButton.tsx
+++ client/src/utils/authHocs/isAuthButton.tsx
@@ -1,26 +1,26 @@
-// src/components/IsAuth/isAuth.tsx
+// // src/components/IsAuth/isAuth.tsx
 
-import { useAuth } from '../context/AuthProviderMerged';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Layout from '../../components/Layout/Layout';
+// import { useAuth } from '../context/AuthProviderMerged';
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
+// import Layout from '../../components/Layout/Layout';
 
-const IsAuth = (WrappedComponent) => (props) => {
-  const { isLoggedIn } = useAuth();
-  const auth = isLoggedIn;
-  const router = useRouter();
+// const IsAuth = (WrappedComponent) => (props) => {
+//   const { isLoggedIn } = useAuth();
+//   const auth = isLoggedIn;
+//   const router = useRouter();
 
-  useEffect(() => {
-    if (!auth) {
-      router.push('/');
-    }
-  }, [auth, router]);
+//   useEffect(() => {
+//     if (!auth) {
+//       router.push('/');
+//     }
+//   }, [auth, router]);
 
-  return (
-    <Layout>
-      <WrappedComponent {...props} />
-    </Layout>
-  );
-};
+//   return (
+//     <Layout>
+//       <WrappedComponent {...props} />
+//     </Layout>
+//   );
+// };
 
-export default IsAuth; // Export IsAuth as a named export
+// export default IsAuth; // Export IsAuth as a named export
