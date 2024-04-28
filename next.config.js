1a250fe64c30495b0f807dfbda64c54a62352765
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2024-04-28 21:19:28 +0200

    next

--- next.config.js
+++ next.config.js
@@ -1,8 +1,24 @@
-// next.config.js
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    basePath: '/client',    // Set base path to match the deployment subfolder
-    assetPrefix: '/client/', // Set asset prefix for assets like images, CSS, etc.
-    // Other configuration options
-  };
-  
+  basePath: '/client',
+  assetPrefix: '/client/',
+  // Other configuration options
+
+  webpack: (config) => {
+    // Copy _redirects file to the build output directory
+    config.plugins.push(
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, '_redirects'),
+            to: path.resolve(__dirname, '.next'),
+          },
+        ],
+      })
+    );
+
+    return config;
+  },
+};
