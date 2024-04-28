aa21c49c25a065228ba5bfc748de15f0d9a0990c
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2024-04-28 21:15:38 +0200

    next

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+
+module.exports = {
+    basePath: '/client',    // Set base path to match the deployment subfolder
+    assetPrefix: '/client/', // Set asset prefix for assets like images, CSS, etc.
+    // Other configuration options
+  };
+  
