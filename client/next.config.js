0d1346fcc17f96e1e3d09580f8f6589a45ab2545
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2024-04-28 21:37:12 +0200

    next

--- client/next.config.js
+++ client/next.config.js
@@ -1,11 +1,31 @@
 /** @type {import('next').NextConfig} */
-
 const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development',
   dest: 'public',
 });
 
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
+
+  webpack: (config, { isServer }) => {
+    // Add CopyPlugin to copy _redirects file
+    if (!isServer) {
+      config.plugins.push(
+        new CopyPlugin({
+          patterns: [
+            {
+              from: path.resolve(__dirname, '_redirects'),
+              to: path.resolve(__dirname, '.next'), // Copy to .next directory
+            },
+          ],
+        })
+      );
+    }
+
+    return config;
+  },
 });
